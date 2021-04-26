4759233e817c9ef5166587599e509137c5759a0b
catalinpit <catalinpit@gmail.com>
2021-04-26 11:17:10 +0300

    Finished the controllers

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -1,24 +1,85 @@
 const client = require("../db/db_config");
 
 const getCourses = async (req, res) => {
-    const allCourses = await client.query('select * from mydb.courses');
+    const allCourses = await client.query('SELECT * FROM mydb.courses');
+
     res.send({ allCourses });
 };
 
-const getSpecificCourse = (req, res) => {
-    res.send({ specific: 'course' });
+const getSpecificCourse = async (req, res) => {
+    const course = await client.query(`SELECT * FROM mydb.courses WHERE user_id=${req.params.name}`);
+
+    res.send({ course });
 };
 
-const addCourse = (req, res) => {
-    res.send({ msg: 'added course!' });
+const addCourse = async (req, res) => {
+    const allProperties = Object.keys(req.body).length;
+
+    if (allProperties !== 4) {
+        res.code(400).send({ 
+            error: 'Some properties are missing! Add the name, description, author and link!' 
+        });
+
+        return;
+    }
+
+    try {
+        const newCourse = await client.insert({
+            table: 'courses',
+            records: [
+                {
+                    name: req.body.name,
+                    description: req.body.description,
+                    author: req.body.author,
+                    link: req.body.link
+                }
+            ]
+        });
+
+        res.send({ newCourse });
+    } catch (error) {
+        res.send({ error });
+    }
 };
 
-const editCourse = (req, res) => {
-    res.send({ msg: 'course edited!' });
+const editCourse = async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUptades = ['name', 'description', 'author', 'link'];
+    const isValidOperation = updates.every((update) => allowedUptades.includes(update));
+
+    if (!isValidOperation) {
+        res.code(400).send({ error: 'Not a valid operation! '});
+
+        return;
+    }
+
+    try {
+        const updatedCourse = await client.update({
+            table: 'courses',
+            records: [
+                {
+                    id: req.params.name,
+                    name: req.body.name,
+                    description: req.body.description,
+                    author: req.body.author,
+                    link: req.body.link
+                }
+            ]
+        });
+
+        res.send({ updatedCourse });
+    } catch (error) {
+        res.send({ error });
+    }
 };
 
-const deleteCourse = (req, res) => {
-    res.send({ msg: 'course deleted!' });
+const deleteCourse = async (req, res) => {
+    const course = await client.query(`DELETE FROM mydb.courses WHERE user_id=${req.params.name}`);
+
+    res.send({ 
+        message: 'Course deleted!',
+        deleteCourse: course
+     });
 };
 
 module.exports = {
